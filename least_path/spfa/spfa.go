334ffd5af52a40216f12cde8c7c09fd197d0396a
southli <961065551@qq.com>
2021-12-09 20:52:45 +0800

    spfa判断负环方法

--- least_path/spfa/spfa.go
+++ least_path/spfa/spfa.go
@@ -3,7 +3,7 @@ package spfa
 const N = 100010
 const INF = 0x3f3f3f3f
 
-var e, ne, w, h, dist [N]int
+var e, ne, w, h, dist, cnt [N]int
 var st [N]bool
 var n, m, idx int
 
@@ -53,4 +53,41 @@ func spfa() int {
 		return INF
 	}
 	return dist[n]
+}
+
+//spfa判断是否存在负环
+func spfa_check_has_circle() bool {
+
+	//判断负环无法确定负环从那个点开始，因此要将所有点全部入队
+	q := []int{}
+	for i := 1; i <= n; i++ {
+		q = append(q, i)
+		st[i] = true
+	}
+
+	for len(q) > 0 {
+		//将队头出队，并标记一下
+		t := q[0]
+		q = q[1:]
+		st[t] = false
+		//从t开始，深度遍历所有节点，确保其目前处于一个最小的状态，如果不是，则将其入队，同时修改其dist
+		for i := h[t]; i != -1; i = ne[i] {
+			j := e[i]
+			if dist[j] > dist[t]+w[i] {
+				dist[j] = dist[t] + w[i]
+
+				//判断负环增加代码如下
+				cnt[j] = cnt[t] + 1
+				if cnt[j] >= n {
+					return true
+				}
+
+				if !st[j] {
+					st[j] = true
+					q = append(q, j)
+				}
+			}
+		}
+	}
+	return false
 }
