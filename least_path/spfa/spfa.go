28c14b554343be7621e32402fe975554edc60c86
southli <961065551@qq.com>
2021-12-09 20:40:50 +0800

    spfa找最短路，可以有重边，可以有负权边

---
+++ least_path/spfa/spfa.go
@@ -0,0 +1,56 @@
+package spfa
+
+const N = 100010
+const INF = 0x3f3f3f3f
+
+var e, ne, w, h, dist [N]int
+var st [N]bool
+var n, m, idx int
+
+//初始化时，将表头赋为-1，dist赋为无穷大
+func ini() {
+	for i := range h {
+		h[i] = -1
+		dist[i] = INF
+	}
+}
+
+//采用邻接表存图，c为边权
+func add(a, b, c int) {
+	e[idx] = b
+	w[idx] = c
+	ne[idx] = h[a]
+	h[a] = idx
+	idx++
+}
+
+func spfa() int {
+	//从点1到点n，因此dist[1] = 0
+	dist[1] = 0
+	q := []int{}
+	//将起点入队
+	q = append(q, 1)
+	//标记起点已经入队
+	st[1] = true
+	for len(q) > 0 {
+		//将队头出队，并标记一下
+		t := q[0]
+		q = q[1:]
+		st[t] = false
+		//从t开始，深度遍历所有节点，确保其目前处于一个最小的状态，如果不是，则将其入队，同时修改其dist
+		for i := h[t]; i != -1; i = ne[i] {
+			j := e[i]
+			if dist[j] > dist[t]+w[i] {
+				dist[j] = dist[t] + w[i]
+				if !st[j] {
+					st[j] = true
+					q = append(q, j)
+				}
+			}
+		}
+	}
+	if dist[n] == INF {
+		return INF
+	}
+	return dist[n]
+}
